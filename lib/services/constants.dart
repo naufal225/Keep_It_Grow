6af65590a48adf29b33e21fa7db4534e56fea5f2
naufal225 <naufalmarufashrori225@gmail.com>
2026-05-06 07:07:27 +0700

    some changes

--- lib/services/constants.dart
+++ lib/services/constants.dart
@@ -1,7 +1,7 @@
 class ServiceConstants {
   // Base URL untuk API backend
-  static const String apiBase = 'http://10.0.2.2:8000/api';
+  static const String apiBase = 'http://192.168.1.9:8000/api';
 
   // Base URL untuk file storage (gambar, bukti, dll.)
-  static const String storageBase = 'http://10.0.2.2:8000/storage/';
+  static const String storageBase = 'http://192.168.1.9:8000/storage/';
 }
